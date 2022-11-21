77053b9dc157c926720ae1da210525fe15be5872
prueba2jai <prueba2jai@gmail.com>
2022-11-21 16:11:42 -0600

    cambio

--- js/init.js
+++ js/init.js
@@ -8,11 +8,12 @@
  * servidor. */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyDzZxk913MUi3ESjLwNK14W0t1c6HgUOWE",
-    authDomain: "bdfalumnox.firebaseapp.com",
-    projectId: "bdfalumnox",
-    storageBucket: "bdfalumnox.appspot.com",
-    messagingSenderId: "1002536188546",
-    appId: "1:1002536188546:web:cfa9fb33aec119e7fc529b",
-    measurementId: "G-5HHSLWQ24J"
+    apiKey: "AIzaSyBAQdKw3yhHmkSv6bEAV0jyYs0vf_tUPiA",
+  authDomain: "prueba2jai1.firebaseapp.com",
+  projectId: "prueba2jai1",
+  storageBucket: "prueba2jai1.appspot.com",
+  messagingSenderId: "1083067937266",
+  appId: "1:1083067937266:web:2d9c8050f96f8544707b6b",
+  measurementId: "G-SL2F04VYYV"
+
 });
